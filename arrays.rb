4a8ff8781741027abac396f6fb8b3a8b8af4a5fd
Thomas Hayes <hayes.thomas610@gmail.com>
2022-06-09 07:28:59 -0600

    Add methods for variables

--- arrays.rb
+++ arrays.rb
@@ -5,3 +5,16 @@ bike_size = 15, 17, 19, 21
 seatpost_size = 27.2, 30.8, 31.6, 34.0
 
 bike_complete = [true, false, false, true]
+
+#identifies last element from array and returns it; this element is now removed in the code (although still exists in variable string)
+  puts bike_brands.pop
+
+#identifies element [0] and returns it, shift all other elements up; this element is now removed in the code (although still exists in variable string)
+  puts bike_size.shift
+
+#allows additon of an element to the end of an array
+  puts bike_brands.push("trek", "specialized")
+
+#allows addition of an element as [0]
+  puts seatpost_size.unshift(24.8)
+  
